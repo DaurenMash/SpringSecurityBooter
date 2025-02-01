a05c2fc9705784b2781a470f2288bbd0b577141f
DaurenMash <doxa@inbox.ru>
2025-02-01 12:57:46 +0300

    V3 commit

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,7 +16,6 @@ import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
-@Data
 
 public class User implements UserDetails {
     @Id
